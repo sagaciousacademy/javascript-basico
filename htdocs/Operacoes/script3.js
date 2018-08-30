fc626afbbf8ae01c62fe3c0a0643bc116b168e69
Marcelo <ajuda@sagaciousacademy.com.br>
2018-08-29 23:27:52 -0300

    Operações Matemáticas

---
+++ htdocs/Operacoes/script3.js
@@ -0,0 +1,8 @@
+function somar() {
+    var campo1 = parseInt(document.getElementById("campo1").value);
+    var campo2 = parseInt(document.getElementById("campo2").value);
+
+    var soma = campo1 + campo2;
+
+    alert(soma);
+}
