6c76b5cf0bc798add413f1d81fc30c9fe6a4d365
Marcelo <ajuda@sagaciousacademy.com.br>
2018-09-01 00:43:01 -0300

    Switches

---
+++ htdocs/Switch/script.js
@@ -0,0 +1,17 @@
+x = prompt("Qual é o número?");
+
+switch (x) {
+    case "0":
+        alert("X é 0");
+        break;
+    case "1":
+        alert("X é 0");
+        break;
+    case "2":
+        alert("X é 2");
+        break;
+    default:
+        alert("Ninguem foi satisfeito!");
+        break;
+
+}
