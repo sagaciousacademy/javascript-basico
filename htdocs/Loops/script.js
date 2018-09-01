c9f05c105d06357dbc094cc64ee9fabd36bdc628
Marcelo <ajuda@sagaciousacademy.com.br>
2018-09-01 00:28:56 -0300

    Loops

---
+++ htdocs/Loops/script.js
@@ -0,0 +1,9 @@
+for(x = 0; x < 10; x++ ) {
+    document.write("Numero "+x+"<br/>");
+}
+
+x = 0;
+while(x < 10) {
+    document.write("Numero "+x+"<br/>");
+    x++;
+}
---
+++ htdocs/Loops/script2.js
@@ -0,0 +1,12 @@
+var x = 0;
+
+document.write("Iniciando o loop...<br/>");
+
+while(x < 10) {
+
+    document.write("Número: "+x+"<br/>");
+
+    x++;
+}
+
+document.write("Finalizando o loop...<br/>");
