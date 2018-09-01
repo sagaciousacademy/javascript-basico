50397127b9763fb42da8b55fc6278a65e7faa986
Marcelo <ajuda@sagaciousacademy.com.br>
2018-09-01 00:05:54 -0300

    Verificadores

---
+++ htdocs/Verificadores/script.js
@@ -0,0 +1,11 @@
+function testeVerificador() {
+    var x = prompt("Diga um número;");
+
+    if (x > 10) {
+
+        alert ("É maior que 10!");
+
+    } else {
+        alert("Não é maior que 10!");
+    }
+}
